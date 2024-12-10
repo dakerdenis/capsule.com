4e5c57c791a8e98f97c68ff5c124f7d254f2f69f
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2024-12-10 13:17:55 +0400

    adde

--- capsule/resources/views/index.blade.php
+++ capsule/resources/views/index.blade.php
@@ -1,41 +1,68 @@
 @extends('layouts.app')
 
-@section('title', 'Welcome to PPF Website')
+@section('title', 'Capsule - car proection')
 
 @section('content')
-<div class="main__wrapper">
-<!---header-->
-    <header class="header" id="header">
-        
-    </header>
+    <div class="main__container">
+        <!---header-->
+        <header class="header" id="header">
 
-    <!---main home section---->
-    <section class="main" id="main">
+        </header>
 
-    </section>
+        <!---main home section---->
+        <section class="main" id="main">
+            <!----text and car--->
+            <div class="main__wrapper">
+                <!---- Name desc button ---->
+                <div class="main__info">
+                    <div class="main__name">
+                        <h1>Premium class</h1>
+                        <h1>Paint Protection Film.</h1>
+                    </div>
+                    <div class="main__desc">
+                        Cutting-edge Paint Protection Film production delivering unmatched quality.
+                        A forward-thinking catalog of clear, colored, and tinted PPFs, powered by advanced nanotechnology
+                        research and innovation.
+                    </div>
+                    <div class="main__buttons">
 
-    <!---About us section---->
-    <section class="about_us" id="about_us">
+                    </div>
+                </div>
 
-    </section>
+                <!---car and description---->
+                <div class="main__car">
+                    <img src="{{asset('./images/car_main.png')}}" alt="Car info image" srcset="">
+                </div>
+            </div>
 
-    <!----Catalogue-->
-    <section class="catalog" id="catalog">
+            <!---main background--->
+            <div class="main__background">
+                <img src="{{ asset('./images/background.png') }}" alt="Car Image">
+            </div>
+        </section>
 
-    </section>
+        <!---About us section---->
+        <section class="about_us" id="about_us">
 
-    <!------Gallery---->
-    <section class="gallery" id="gallery">
+        </section>
 
-    </section>
+        <!----Catalogue-->
+        <section class="catalog" id="catalog">
 
-    <!---COntact US----->
-    <section class="contact" id="contact">
+        </section>
 
-    </section>
+        <!------Gallery---->
+        <section class="gallery" id="gallery">
 
-    <footer>
-        <p>&copy; {{ date('Y') }} PPF Website. All rights reserved.</p>
-    </footer>
-</div>
+        </section>
+
+        <!---COntact US----->
+        <section class="contact" id="contact">
+
+        </section>
+
+        <footer>
+            <p>&copy; {{ date('Y') }} PPF Website. All rights reserved.</p>
+        </footer>
+    </div>
 @endsection
