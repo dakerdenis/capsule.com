80ad454e2acf060c5786f407c8fdfce34fe7d489
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2024-12-16 14:13:59 +0400

    added

--- capsule/resources/views/layouts/app.blade.php
+++ capsule/resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
     <!DOCTYPE html>
     <html lang="en">
+
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,21 +8,29 @@
 
 
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@glidejs/glide/dist/css/glide.core.min.css">
-<script src="https://cdn.jsdelivr.net/npm/@glidejs/glide/dist/glide.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@glidejs/glide/dist/glide.min.js"></script>
 
 
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css" />
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/ui/dist/fancybox.css" />
-      
 
 
+        <script src="https://ajax.googleapis.com/ajax/libs/cesiumjs/1.78/Build/Cesium/Cesium.js"></script>
+        <!-- jQuery Library -->
+        <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+        <!-- Select2 Library -->
+        <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/css/select2.min.css" rel="stylesheet" />
+        <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"></script>
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
+
     <body>
 
-            @yield('content')
+        @yield('content')
 
-            <script src="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js"></script>
         <script src="{{ asset('js/app.js') }}" defer></script>
     </body>
+
     </html>
