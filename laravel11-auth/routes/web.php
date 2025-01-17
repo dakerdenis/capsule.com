b642d6ccf17460bb262f152d252bbf7272541dee
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2025-01-17 18:06:50 +0400

    added

--- laravel11-auth/routes/web.php
+++ laravel11-auth/routes/web.php
@@ -5,6 +5,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\WarrantyController;
+use App\Http\Controllers\VerificationController;
+use App\Http\Controllers\AdminProductsController;
+use App\Http\Controllers\AdminServicesController;
+use App\Http\Controllers\AdminWarrantyController;
 
 //! MAIN PAGE ROUTES + CATALOG
 //*************************** */
@@ -26,7 +30,8 @@ Route::get('/catalog', fn() => redirect('/en/catalog'));
 //!Verification Page for all users -> need to add maximum values of attempts per minute.
 //*************************** */
 //*************************** */
-Route::get('/verification', [PageController::class, 'verification'])->name('verification');
+Route::get('/verification', [VerificationController::class, 'verification'])->name('verification');
+Route::post('/verification', [VerificationController::class, 'verification_check'])->name('verification.post');
 //*************************** */
 //*************************** */
 
@@ -44,65 +49,53 @@ Route::post('/admin/logout', [AdminController::class, 'logout'])->name('admin.lo
 
 
 //* Admin authenticated MIddleware (ONLY AUTHORIZED ADMIN CAN ACCESS)
+//*************************** */
 Route::middleware('auth_admin')->group(function () {
     //dashboard admin page - admin panel only could be accessed by authenticated adminstrators
     Route::get('/admin', [AdminController::class, 'showAdminPage'])->name('admin.dashboard');
     //PRODUCTS + SERVICES + WARANTIES
     Route::prefix('admin')->group(function () {
         //*PRODUCTS
-        Route::get('/products', [AdminController::class, 'adminProducts'])->name('admin.products');
+        Route::get('/products', [AdminProductsController::class, 'adminProducts'])->name('admin.products');
         Route::prefix('products')->group(function () {
-            Route::get('/add', [AdminController::class, 'adminProductAdd'])->name('admin.add_product');
-            Route::post('/add', [AdminController::class, 'adminPostProductAdd'])->name('admin.add_post_product');
-            Route::delete('/delete/{id}', [AdminController::class, 'adminDeleteProduct'])->name('admin.delete_product');
+            Route::get('/add', [AdminProductsController::class, 'adminProductAdd'])->name('admin.add_product');
+            Route::post('/add', [AdminProductsController::class, 'adminPostProductAdd'])->name('admin.add_post_product');
+            Route::delete('/delete/{id}', [AdminProductsController::class, 'adminDeleteProduct'])->name('admin.delete_product');
         });
         //* SERVICES
-        Route::get('/services', [AdminController::class, 'adminServices'])->name('admin.services');
+        Route::get('/services', [AdminServicesController::class, 'adminServices'])->name('admin.services');
         Route::prefix('services')->group(function () {
-            Route::get('/{id}', [AdminController::class, 'adminSingleService'])->name('admin.service');
-            Route::get('/{id}/edit', [AdminController::class, 'adminEditService'])->name('admin.edit_service');
-            Route::post('/{id}/edit', [AdminController::class, 'adminPostEditService'])->name('admin.edit_post_service');
-            Route::delete('/{id}', [AdminController::class, 'adminDeleteService'])->name('admin.delete_service');
-            Route::get('/add', [AdminController::class, 'adminNewService'])->name('admin.new_service');
-            Route::post('/add', [AdminController::class, 'adminPostNewService'])->name('admin.new_post_service');
+            Route::get('/{id}', [AdminServicesController::class, 'adminSingleService'])->name('admin.service');
+            Route::get('/{id}/edit', [AdminServicesController::class, 'adminEditService'])->name('admin.edit_service');
+            Route::post('/{id}/edit', [AdminServicesController::class, 'adminPostEditService'])->name('admin.edit_post_service');
+            Route::delete('/{id}', [AdminServicesController::class, 'adminDeleteService'])->name('admin.delete_service');
+            Route::get('/add', [AdminServicesController::class, 'adminNewService'])->name('admin.new_service');
+            Route::post('/add', [AdminServicesController::class, 'adminPostNewService'])->name('admin.new_post_service');
         });
         //* WARRANTIES
-        Route::get('/warranties', [AdminController::class, 'adminWarranties'])->name('admin.warranties');
+        Route::get('/warranties', [AdminWarrantyController::class, 'adminWarranties'])->name('admin.warranties');
         Route::prefix('warranties')->group(function () {
-            Route::get('/{id}', [AdminController::class, 'adminSingleWarranty'])->name('admin.warranty');
-            Route::get('/{id}/edit', [AdminController::class, 'adminSingleWarranty'])->name('admin.edit_warranty');
-            Route::post('/{id}', [AdminController::class, 'adminEditPostWarranty'])->name('admin.edit_post_warranty');
+            Route::get('/{id}', [AdminWarrantyController::class, 'adminSingleWarranty'])->name('admin.warranty');
+            Route::get('/{id}/edit', [AdminWarrantyController::class, 'adminSingleWarranty'])->name('admin.edit_warranty');
+            Route::post('/{id}', [AdminWarrantyController::class, 'adminEditPostWarranty'])->name('admin.edit_post_warranty');
         });
     });
 });
+//*************************** */
 
+//*Warranty system for Services
+//*************************** */
 Route::get('/warranty', [WarrantyController::class, 'warrantyPage'])->name('service.login');
-Route::middleware('auth_service')->group(function () {});
-
-
-// Warranty System Routes
-
-
-
-//Login page for basic users
-Route::get('/warranty', function () {
-    return 'USer login page';
-})->name('user.login');
-Route::post('/warranty', function () {
-    return 'USer login page';
-})->name('user.login_form');
-
-
-//PAge could access only by authenticated users
-Route::get('/warranty/document', function () {
-    return 'USer document page';
+Route::post('/warranty',[WarrantyController::class,'warrantyLogin'])->name('service.post_login');
+Route::middleware('auth_service')->group(function () {
+    Route::prefix('warranty')->group(function(){
+        Route::get('/register', [WarrantyController::class, 'warrantyregister'])->name('service.register');
+        Route::post('/register', [WarrantyController::class, 'warrantyPostRegister'])->name('service.post_register');
+    });
 });
 
-
-//PAge could be access by evryone.
-Route::get('/warranty/{id}', function (Request $request, string $id) {
-    return 'Document ' . $id;
-});
+Route::get('/warranty/{id}', [WarrantyController::class, 'singleWarranty'])->name('service.warranty');
+//*************************** */
 
 
 Route::fallback(function () {
