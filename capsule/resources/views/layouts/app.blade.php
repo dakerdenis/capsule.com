19565c0c0a3859bbb0e09958a398f7591466fba1
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2025-01-23 10:12:23 +0400

    added

--- capsule/resources/views/layouts/app.blade.php
+++ capsule/resources/views/layouts/app.blade.php
@@ -4,17 +4,17 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Capsule - car protection</title>
+        <title>Capsule PPF | High-quality car protection</title>
 
         <!-- Primary Meta Tags -->
-        <meta name="description" content="@yield('meta_description', 'Capsule PPF provides high-quality protective films for cars, offering superior protection against scratches, chemical exposure, and damage while ensuring a glossy, transparent finish. Maintain your car’s aesthetics with ease.')">
+        <meta name="description" content="Capsule PPF provides high-quality protective films for cars, offering superior protection against scratches, chemical exposure, and damage while ensuring a glossy, transparent finish. Maintain your car’s aesthetics with ease.">
         <meta name="keywords" content="Capsule PPF, car protection film, paint protection, polyurethane film, self-healing film, car aesthetics, scratch-resistant film">
         <meta name="author" content="Capsule PPF">
         <link rel="canonical" href="{{ url()->current() }}">
     
         <!-- Open Graph Meta Tags -->
-        <meta property="og:title" content="@yield('og_title', 'Capsule - Car Protection with PPF')">
-        <meta property="og:description" content="@yield('og_description', 'Capsule PPF offers premium car protection with advanced polyurethane films featuring self-healing, water-repellent, and long-lasting durability.')">
+        <meta property="og:title" content="Capsule - Car Protection with PPF">
+        <meta property="og:description" content="Capsule PPF offers premium car protection with advanced polyurethane films featuring self-healing, water-repellent, and long-lasting durability.">
         <meta property="og:type" content="website">
         <meta property="og:url" content="{{ url()->current() }}">
         <meta property="og:image" content="{{ asset('public/images/logo_main.png') }}">
