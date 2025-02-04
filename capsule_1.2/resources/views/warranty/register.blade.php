5a4cca1d0797daae909f6c04fd25b0f02ad58313
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2025-02-04 16:37:04 +0400

    added

--- capsule_1.2/resources/views/warranty/register.blade.php
+++ capsule_1.2/resources/views/warranty/register.blade.php
@@ -30,11 +30,16 @@
             <div class="form-row">
                 <div class="form-group">
                     <label for="client-name">Client Name:</label>
-                    <input type="text" id="client-name" name="client_name" placeholder="Enter client name" required>
+                    <div class="warranty-input-container">
+                        <input type="text" id="client-name" name="client_name" placeholder="Enter client name" required>
+                    </div>
                 </div>
                 <div class="form-group">
                     <label for="client-phone">Client Phone Number:</label>
-                    <input type="text" id="client-phone" name="client_phone" placeholder="Enter client phone number" required>
+                    <div class="warranty-input-container">
+                        <input type="text" id="client-phone" name="client_phone" placeholder="Enter client phone number" required>
+                    </div>
+                   
                 </div>
             </div>
     
@@ -42,29 +47,42 @@
             <div class="form-row">
                 <div class="form-group">
                     <label for="car-model">Car Model:</label>
-                    <input type="text" id="car-model" name="car_model" placeholder="Enter car model" required>
+                    <div class="warranty-input-container">
+                        <input type="text" id="car-model" name="car_model" placeholder="Enter car model" required>
+                    </div>
+                    
                 </div>
                 <div class="form-group">
                     <label for="car-make">Car Make:</label>
-                    <input type="text" id="car-make" name="car_make" placeholder="Enter car make" required>
+                    <div class="warranty-input-container">
+                        <input type="text" id="car-make" name="car_make" placeholder="Enter car make" required>
+                    </div>
+                    
                 </div>
             </div>
     
             <div class="form-row">
                 <div class="form-group">
                     <label for="car-color">Car Color:</label>
-                    <input type="text" id="car-color" name="car_color" placeholder="Enter car color" required>
+                    <div class="warranty-input-container">
+                        <input type="text" id="car-color" name="car_color" placeholder="Enter car color" required>
+                    </div>
+                    
                 </div>
                 <div class="form-group">
                     <label for="car-year">Year of Manufacture:</label>
+                    <div class="warranty-input-container">
                     <input type="text" id="car-year" name="car_year" placeholder="Enter year of manufacture" required>
+                    </div>
                 </div>
             </div>
     
             <div class="form-row">
                 <div class="form-group">
                     <label for="license-plate">License Plate Number:</label>
+                    <div class="warranty-input-container">
                     <input type="text" id="license-plate" name="license_plate" placeholder="Enter license plate number" required>
+                    </div>
                 </div>
             </div>
     
@@ -72,67 +90,92 @@
             <div class="form-row">
                 <div class="form-group non-editable">
                     <label for="service-name">Service Name:</label>
+                    <div class="warranty-input-container">
                     <input type="text" id="service-name" name="service_name" value="{{ $service->name }}" readonly>
+                    </div>
                 </div>
                 <div class="form-group">
                     <label for="manager-name">Manager/Master Name:</label>
+                    <div class="warranty-input-container">
                     <input type="text" id="manager-name" name="manager_name" placeholder="Enter manager name" required>
+                    </div>
                 </div>
             </div>
     
             <div class="form-row">
                 <div class="form-group non-editable">
                     <label for="service-phone">Service Phone Number:</label>
+                    <div class="warranty-input-container">
                     <input type="text" id="service-phone" name="service_phone" value="{{ $service->phone }}" readonly>
+                    </div>
                 </div>
                 <div class="form-group non-editable">
                     <label for="license-number">License Number:</label>
+                    <div class="warranty-input-container">
                     <input type="text" id="license-number" name="license_number" value="{{ $licenseNumber }}" readonly>
+                    </div>
                 </div>
             </div>
     
             <div class="form-group non-editable">
                 <label for="installation-date">Installation Date:</label>
+                <div class="warranty-input-container">
                 <input type="text" id="installation-date" name="installation_date" value="{{ $installationDate }}" readonly>
+                </div>
             </div>
     
             <h2 class="warranty__name-name">Film and Warranty Information</h2>
             <div class="form-row">
                 <div class="form-group non-editable">
                     <label for="brand-name">Brand Name:</label>
+                    <div class="warranty-input-container">
                     <input type="text" id="brand-name" name="brand_name" value="Capsule" readonly>
+                    </div>
                 </div>
                 <div class="form-group non-editable">
                     <label for="film-model">Film Model:</label>
+                    <div class="warranty-input-container">
                     <input type="text" id="film-model" name="film_model" value="{{ $filmModel }}" readonly>
+                    </div>
                 </div>
             </div>
     
             <div class="form-row">
                 <div class="form-group non-editable">
                     <label for="warranty-period">Warranty Period:</label>
-                    <input type="text" id="warranty-period" name="warranty_period" value="{{ $warrantyPeriod }}" readonly>
-                </div>
+                    <div class="warranty-input-container">
+                
+                        <input type="text" id="warranty-period" name="warranty_period" value="{{ $warrantyPeriod }}" readonly>
+                    </div>
+                    </div>
                 <div class="form-group non-editable">
                     <label for="service-life">Lifespan:</label>
+                    <div class="warranty-input-container">
                     <input type="text" id="service-life" name="service-life" value="{{ $serviceLife }} Years" readonly>
+                    </div>
                 </div>
             </div>
     
             <div class="form-group non-editable">
                 <label for="warranty-end-date">Warranty End Date:</label>
+                <div class="warranty-input-container">
                 <input type="text" id="warranty-end-date" name="warranty_end_date" value="{{ $warrantyEndDate }}" readonly>
+                </div>
             </div>
     
             <div class="form-group non-editable">
                 <label for="client-code">Client/Document Code:</label>
+                <div class="warranty-input-container">
                 <input type="text" id="client-code" name="client_code" value="{{ $clientCode }}" readonly>
+                </div>
             </div>
     
             <div class="form-group photo-upload">
                 <label for="installation-photos">Installation Photos:</label>
+                <div class="warranty-input-container">
                 <input type="file" id="installation-photos" name="installation_photos[]" multiple accept="image/*">
-            </div>
+                </div>
+                </div>
     
             <button type="submit" class="submit-btn">Submit</button>
         </form>
