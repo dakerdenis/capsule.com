f18a91db58a316e9cbe41f2ed1cf7219ab1a93f1
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2025-02-21 14:39:30 +0400

    added

--- capsule_1.3/resources/views/index.blade.php
+++ capsule_1.3/resources/views/index.blade.php
@@ -6,14 +6,77 @@
 
 
     <div class="main__wrapper">
-        <header>
+        <!----page's header--->
+        <header class="header">
+            <div class="header__wrapper">
+                <!---HEADER LOGO---->
+                <div class="header__logo">
+                    <picture>
+                        <source srcset="{{ asset('images/logo-large.webp') }}" media="(min-width: 768px)" type="image/webp">
+                        <source srcset="{{ asset('images/logo-small.webp') }}" media="(max-width: 767px)" type="image/webp">
 
+                        <img src="{{ asset('images/logo-large.webp') }}" alt="Capsule Logo" width="338" height="113">
+                    </picture>
+                </div>
+
+                <!--HEADER navigation---->
+                <div class="header__navigation">
+
+                </div>
+
+                <!---header language--->
+                <div class="haeder__languge">
+
+                </div>
+            </div>
         </header>
 
 
         <!---section HOME--->
         <section class="main__section" id="home">
+            <div class="home__wrapper">
+                <!--home background-->
+                <div class="home__background">
+                    <picture>
+                        <source srcset="{{ asset('images/background_home-large.webp') }}" media="(min-width: 768px)"
+                            type="image/webp">
+                        <source srcset="{{ asset('images/background_home-small.webp') }}" media="(max-width: 767px)"
+                            type="image/webp">
 
+                        <img src="{{ asset('images/background_home-large.webp') }}" alt="Capsule Background">
+                    </picture>
+                </div>
+
+                <!---home desc and car--->
+                <div class="home__desc__car">
+                    <div class="home__desc">
+                        <div class="home__desc__name">
+                            <h1> High-End Paint <br> Protection Film</h1>
+                        </div>
+
+                        <div class="home__desc__text">
+                            <p>Advanced production of protective films for automotive paint, ensuring unmatched quality and
+                                reliability. A diverse range of clear and colored PPF films developed using the latest
+                                nanotechnology research and innovative solutions.</p>
+                        </div>
+
+                        <div class="home__desc__button">
+                            <button id="contact_button">
+                                contact us now
+                            </button>
+
+                            <button id="lear_more_button">
+                                Learn more
+                            </button>
+                        </div>
+                    </div>
+
+                    <div class="home__car">
+
+                    </div>
+                </div>
+
+            </div>
         </section>
 
         <!---section ABOUT--->
@@ -41,11 +104,11 @@
         <section class="gallery__section" id="gallery">
 
         </section>
-    </div>
 
-    <section>
-        <h2>Welcome to Capsule</h2>
-        <p>Capsule is the future of premium protection.</p>
-        <a href="#">Learn More</a>
-    </section>
+
+        <!---section Catalog--->
+        <section class="contact__section" id="contact">
+
+        </section>
+    </div>
 @endsection
